9db581f048129cd3b7f595f98bf00322cefcbbea
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-02 15:22:32 +0530

    Refactored : Calculating the Prime Factors of a number

--- src/FactorsOfNumber.java
+++ src/FactorsOfNumber.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class FactorsOfNumber {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
         int num = sc.nextInt();
 
         for (int i = 1; i < num; i++) {
