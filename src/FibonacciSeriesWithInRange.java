4cf45134a310ca1f5740115f9018c5b48b82b61e
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-02 14:59:04 +0530

    Refactored : Finding the Fibonacci series with in a range

---
+++ src/FibonacciSeriesWithInRange.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FibonacciSeriesWithInRange {
+
+    public static void main(String[] args) {
+
+
+        int a = 0, b = 1, number, NextTerm;
+
+        System.out.println("Please Enter which number you want to check");
+
+        Scanner sc = new Scanner(System.in);
+
+        number = sc.nextInt();
+        System.out.print(a + " , " + b);
+
+        for (int i = 0; i <= number; i++) {
+            NextTerm = a + b;
+            a = b;
+            b = NextTerm;
+            System.out.print(" , " + NextTerm);
+
+        }
+
+    }
+
+}
+
+        
